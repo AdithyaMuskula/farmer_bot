612c5df79f42e96e54b5f9bb277ea95d90ad09e8
AdithyaMuskula <adithyamuskula@gmail.com>
2026-04-23 12:15:32 +0530

    Added rag_loader for MCP retrieval

---
+++ rag_loader.py
@@ -0,0 +1,48 @@
+# rag_loader.py
+
+import pickle
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.retrievers import BM25Retriever
+
+
+# -----------------------------
+# LOAD FAISS
+# -----------------------------
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+db = FAISS.load_local(
+    "faiss_index",
+    embeddings,
+    allow_dangerous_deserialization=True
+)
+
+vector_retriever = db.as_retriever(search_kwargs={"k": 3})
+
+
+# -----------------------------
+# LOAD BM25
+# -----------------------------
+with open("splits.pkl", "rb") as f:
+    splits = pickle.load(f)
+
+bm25 = BM25Retriever.from_documents(splits)
+bm25.k = 5
+
+
+# -----------------------------
+# HYBRID FUNCTION
+# -----------------------------
+def hybrid_search(query):
+
+    vector_docs = vector_retriever.invoke(query)
+    bm25_docs = bm25.invoke(query)
+
+    all_docs = vector_docs + bm25_docs
+
+    # remove duplicates
+    unique_docs = list({d.page_content: d for d in all_docs}.values())
+
+    return unique_docs[:5]
